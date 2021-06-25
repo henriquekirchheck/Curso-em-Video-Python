cefa5f1ddf0c87d7a427ad57ba746d571524f0fc
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-06-25 12:16:20 -0300

    Update desafio078.py

--- Aulas/Desafio/desafio078.py
+++ Aulas/Desafio/desafio078.py
@@ -1,12 +1,20 @@
 # Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 value = list()
+min = list()
+max = list()
 
-for x in range(0,5):
-    value.append(int(input('Digite um valor: ')))
+for loop in range(0,5):
+    value.append(int(input(f'Digite um valor no indice {loop}: ')))
 
 cvalue = value.copy()
 cvalue.sort()
 
+for x, y in enumerate(value):
+    if(y == cvalue[0]):
+        min.append(x)
+    elif(y == cvalue[-1]):
+        max.append(x)
+
 print('-' * 30)
-print(f'O maior valor é {cvalue[-1]} e foi encontrado no indice {value.index(cvalue[-1])}')
-print(f'O menor valor é {cvalue[0]} e foi encontrado no indice {value.index(cvalue[0])}')
+print(f'O maior valor é {cvalue[-1]} e foi encontrado nos indices {max}')
+print(f'O menor valor é {cvalue[0]} e foi encontrado nos indices {min}')
