1c9e3d7a22f55bf25a197ed672e08206e93d7d31
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-06-27 22:44:15 -0300

    Create aula004.py

---
+++ Aulas/Aula/aula004.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+#Aula N°4
+
+print("Aula N°4 \n")
+sleep(0.2)
+
+nome = input('Qual é o seu nome: ')
+idade = input('Qual é a sua idade: ')
+peso = input('Qual é o seu peso: ')
+
+print(nome, idade, peso)
