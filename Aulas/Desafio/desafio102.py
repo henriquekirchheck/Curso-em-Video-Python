4ff571a26adaf89f9da1b7a52819eacf35ffa991
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-06-28 16:44:27 -0300

    Update desafio102.py

--- Aulas/Desafio/desafio102.py
+++ Aulas/Desafio/desafio102.py
@@ -1,6 +1,12 @@
 # Crie um programa que tenha uma função fatorial() que receba dois parâmetros: o primeiro que indique o número a calcular e outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo de cálculo do fatorial.
 
 def fatorial(num:int, show:bool = False):
+    """
+    Feito para dar o fatorial de um numero
+    :param num: O numero que você quer fatoriado
+    :param show: Se você quer ou não ser mostrado a logica de como essa conta foi feita
+    :return: Retorna os valores
+    """
     result = 1
     lst = list()
     if(show == False):
@@ -17,5 +23,6 @@ def fatorial(num:int, show:bool = False):
                 lst.append('=')
         lst.append(str(result))
         return (' '.join(lst))
+
 
 print(fatorial(128, True))
