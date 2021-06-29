e4122aad73d6c83a6b6dbac4d08226def0d5599e
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-06-29 20:08:13 -0300

    Create desafio108.py

---
+++ desafio/desafio108.py
@@ -0,0 +1,6 @@
+from utils import moeda
+
+print(moeda.aumentar(100, 50, True))
+print(moeda.diminuir(100, 50, True))
+print(moeda.dobro(100, True))
+print(moeda.metade(100, True))
--- desafio/utils/moeda.py
+++ desafio/utils/moeda.py
@@ -1,26 +1,39 @@
-def metade(valor:int):
+def metade(valor:int, formatado:bool = False):
     """
     Retorna a metade do valor que você botou
     :param valor: o valor que você quer a metade
+    :param formatado: Se você quer que o valor estaja formatado para R$
     :return: A metade do valor
     """
     valor /= 2
+
+    if(formatado == True):
+        valorformatado = ('R${:.2f}'.format(valor))
+        valor = valorformatado.replace('.', ',')
+
     return valor
 
-def dobro(valor:int):
+def dobro(valor:int, formatado:bool = False):
     """
     Retorna o dobro do valor que você botou
     :param valor: o valor que você quer o dobro
+    :param formatado: Se você quer que o valor estaja formatado para R$
     :return: O dobro do valor
     """
     valor *= 2
+
+    if(formatado == True):
+        valorformatado = ('R${:.2f}'.format(valor))
+        valor = valorformatado.replace('.', ',')
+
     return valor
 
-def aumentar(valor:int, porcentagem:int):
+def aumentar(valor:int, porcentagem:int, formatado:bool = False):
     """
     Retorna o valor que você colocou com a porcentagem de aumento
     :param valor: O valor que você quer aumentar uma porcentagem
     :param porcentagem: A porcentagem que você quer aumentar
+    :param formatado: Se você quer que o valor estaja formatado para R$
     :return: O valor já aumentado
     """
 
@@ -28,17 +41,26 @@ def aumentar(valor:int, porcentagem:int):
     porcentagem += 1
     valor *= porcentagem
 
+    if(formatado == True):
+        valorformatado = ('R${:.2f}'.format(valor))
+        valor = valorformatado.replace('.', ',')
+
     return valor
 
-def diminuir(valor:int, porcentagem:int):
+def diminuir(valor:int, porcentagem:int, formatado:bool = False):
     """
     Retorna o valor que você colocou com a porcentagem de diminuição
     :param valor: O valor que você quer diminuir uma porcentagem
     :param porcentagem: A porcentagem que você quer diminuir
+    :param formatado: Se você quer que o valor estaja formatado para R$
     :return: O valor já diminuido
     """
 
     porcentagem /= 100
     valor *= porcentagem
+
+    if(formatado == True):
+        valorformatado = ('R${:.2f}'.format(valor))
+        valor = valorformatado.replace('.', ',')
 
     return valor
