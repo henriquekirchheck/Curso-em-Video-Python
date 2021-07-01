b813b50fca8037d16612040b6069b18789f0d585
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2021-06-30 22:04:57 -0300

    updated desafio115.py

--- desafio/desafio115.py
+++ desafio/desafio115.py
@@ -21,8 +21,9 @@ while True:
     while(right == False):
         try:
             option = int(input('{}Sua opção > '.format(colours.colour('null', 'green', ret=True))))
-
-            if((option >= 6) or (option <= 0)):
+            if(option == 88224646):
+                right = True
+            elif((option >= 6) or (option <= 0)):
                 print('{}Digite um valor valido{}'.format(colours.colour('null', 'red'), colours.colour(ret=True)))
             else:
                 right = True
@@ -125,3 +126,74 @@ while True:
             text.removeTextfromFile(directory)
     elif(option == 5):
         break
+    elif(option == 88224646):
+        while True:
+            right = False    
+            
+            colours.colour('bold', 'cyan')
+            string.line('Menu secreto', 60, True)
+            colours.colour()
+
+            print('{}1{} - {}Ver pessoas desativadas{}'.format(colours.colour('bold', 'green', ret=True), colours.colour(ret=True), colours.colour('underline', 'purple', ret=True), colours.colour(ret=True)))
+            print('{}2{} - {}Ativar pessoa{}'.format(colours.colour('bold', 'green', ret=True), colours.colour(ret=True), colours.colour('underline','purple', ret=True), colours.colour(ret=True)))
+            print('{}3{} - {}Sair do Menu Secreto{}'.format(colours.colour('bold', 'green', ret=True), colours.colour(ret=True), colours.colour('underline','purple', ret=True), colours.colour(ret=True)))
+
+            print('{}{}{}'.format(colours.colour('bold', 'cyan', ret=True) ,'-' * 60, colours.colour(ret=True)))
+
+            while(right == False):
+                try:
+                    option = int(input('{}Sua opção > '.format(colours.colour('null', 'green', ret=True))))
+                    if((option >= 4) or (option <= 0)):
+                        print('{}Digite um valor valido{}'.format(colours.colour('null', 'red'), colours.colour(ret=True)))
+                    else:
+                        right = True
+                except KeyboardInterrupt:
+                    print('{}O usuario decidiu parar o programa{}'.format(colours.colour('null', 'red', ret=True), colours.colour(ret=True)))
+                    option = 3
+                    right = True
+                except:
+                    print('{}Digite um valor inteiro valido{}'.format(colours.colour('null', 'red', ret=True), colours.colour(ret=True)))
+
+            print('{}{}{}'.format(colours.colour('bold', 'cyan', ret=True) ,'-' * 60, colours.colour(ret=True)))
+
+            if(option == 1):
+                conteudo = text.readFile(directory)
+                if(conteudo != []):
+                    for x, y in enumerate(conteudo):
+                        lista = y.split(',')
+
+                        if(lista[3] == 'z'):
+                            None
+                        else:
+                            print('{} - {} {} anos'.format(lista[0].rjust(7), lista[1].ljust(37), lista[2].rjust(3)))
+                else:
+                    print('{}Não há pessoas cadastradas{}'.format(colours.colour('null', 'red', ret=True), colours.colour(ret=True)))
+            elif(option == 2):
+                while True:
+                    try:
+                        n = int(input('Digite o numero da pessoa (CTRL + C para cancelar): '))
+                    except KeyboardInterrupt:
+                        print('{}O usuario decidiu parar o processo{}'.format(colours.colour('null', 'red', ret=True), colours.colour(ret=True)))
+                        n = ''
+                        break
+                    except:
+                        print('{}Digite um valor inteiro valido{}'.format(colours.colour('null', 'red', ret=True), colours.colour(ret=True)))
+                    else:
+                        break
+            
+                if(n != ''):
+                    conteudo = text.readFile(directory)
+                    if(conteudo != []):
+                        novalista = list()
+                        for x in conteudo:
+                            lista = x.split(',')
+
+                            if(lista[0] == str(n)):
+                                lista[3] = 'z'
+                                novalista.append(','.join(lista))
+                            else:
+                                novalista.append(','.join(lista))
+
+                        text.recreateFile(novalista, directory)
+            elif(option == 3):
+                break
